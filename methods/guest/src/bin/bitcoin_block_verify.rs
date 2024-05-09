c3cfd55150624faa785dbb22eb55253aebe20248
shaorongqiang <shaorongqiang@email.com>
2024-05-09 22:24:09 +0800

    fix guest

--- methods/guest/src/bin/bitcoin_block_verify.rs
+++ methods/guest/src/bin/bitcoin_block_verify.rs
@@ -5,6 +5,7 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 use bitcoin_spv::{types::HeaderArray, validatespv::validate_header_chain};
+use ethabi::{ethereum_types::U256, Token};
 use risc0_zkvm::guest::env;
 risc0_zkvm::guest::entry!(main);
 
@@ -13,6 +14,10 @@ fn main() {
     let headers = HeaderArray::new(&data.1).unwrap();
     validate_header_chain(&headers, true).unwrap();
     let raw_header = headers.index(headers.len() - 1);
-    let ret = (data.0, raw_header.digest().as_ref().clone());
+    let hash = raw_header.digest().as_ref().clone();
+    let ret = ethabi::encode(&[
+        Token::Uint(U256::from(data.0)),
+        Token::FixedBytes(hash.to_vec()),
+    ]);
     env::commit(&ret);
 }
