095d3bfb668f736d5ddc42a5de0e71d2e778259a
shaorongqiang <shaorongqiang@email.com>
2024-05-09 22:43:50 +0800

    fix guest

--- methods/guest/src/bin/bitcoin_block_verify.rs
+++ methods/guest/src/bin/bitcoin_block_verify.rs
@@ -1,23 +1,22 @@
 #![no_main]
-#![no_std]
 
 extern crate alloc;
 
 use alloc::vec::Vec;
+use alloy_primitives::U256;
+use alloy_sol_types::SolValue;
 use bitcoin_spv::{types::HeaderArray, validatespv::validate_header_chain};
-use ethabi::{ethereum_types::U256, Token};
 use risc0_zkvm::guest::env;
 risc0_zkvm::guest::entry!(main);
 
 fn main() {
-    let data: (u64, Vec<u8>) = env::read();
-    let headers = HeaderArray::new(&data.1).unwrap();
+    let (height, headers): (u64, Vec<u8>) = env::read();
+    let headers = HeaderArray::new(&headers).unwrap();
     validate_header_chain(&headers, true).unwrap();
     let raw_header = headers.index(headers.len() - 1);
     let hash = raw_header.digest().as_ref().clone();
-    let ret = ethabi::encode(&[
-        Token::Uint(U256::from(data.0)),
-        Token::FixedBytes(hash.to_vec()),
-    ]);
+
+    let ret = (U256::from(height), hash.to_vec()).abi_encode();
+
     env::commit(&ret);
 }
