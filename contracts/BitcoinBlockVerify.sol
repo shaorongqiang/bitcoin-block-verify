ef5be8e56747dc90ab5049b072e52f5ae5c9610f
shaorongqiang <shaorongqiang@email.com>
2024-05-09 16:43:31 +0800

    fix contract

--- contracts/BitcoinBlockVerify.sol
+++ contracts/BitcoinBlockVerify.sol
@@ -23,15 +23,16 @@ import {ImageID} from "./ImageID.sol"; // auto-generated contract after running 
 /// @notice This basic application holds a number, guaranteed to be even.
 /// @dev This contract demonstrates one pattern for offloading the computation of an expensive
 ///      or difficult to implement function to a RISC Zero guest running on Bonsai.
-contract EvenNumber {
+contract BitcoinBlockVerify {
     /// @notice RISC Zero verifier contract address.
     IRiscZeroVerifier public immutable verifier;
     /// @notice Image ID of the only zkVM binary to accept verification from.
-    bytes32 public constant imageId = ImageID.IS_EVEN_ID;
+    bytes32 public constant imageId = ImageID.BITCOIN_BLOCK_VERIFY_ID;
 
     /// @notice A number that is guaranteed, by the RISC Zero zkVM, to be even.
     ///         It can be set by calling the `set` function.
-    uint256 public number;
+    uint256 public height;
+    bytes32 public block_hash;
 
     /// @notice Initialize the contract, binding it to a specified RISC Zero verifier.
     constructor(IRiscZeroVerifier _verifier) {
@@ -40,11 +41,19 @@ contract EvenNumber {
     }
 
     /// @notice Set the even number stored on the contract. Requires a RISC Zero proof that the number is even.
-    function set(uint256 x, bytes32 postStateDigest, bytes calldata seal) public {
+    function set(
+        uint256 height_,
+        bytes32 hash_,
+        bytes32 postStateDigest,
+        bytes calldata seal
+    ) public {
         // Construct the expected journal data. Verify will fail if journal does not match.
-        bytes memory journal = abi.encode(x);
-        require(verifier.verify(seal, imageId, postStateDigest, sha256(journal)));
-        number = x;
+        bytes memory journal = abi.encode(height_, hash_);
+        require(
+            verifier.verify(seal, imageId, postStateDigest, sha256(journal))
+        );
+        height = height_;
+        block_hash = hash_;
     }
 
     /// @notice Returns the number stored.
